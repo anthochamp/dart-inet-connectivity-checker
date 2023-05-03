1083a411b6c4053c17bd08e2b9a12564d969c3a4
Anthony Champagne <contact@anthonychampagne.fr>
2023-05-03 20:28:43 +0200

    add example

---
+++ example/example.dart
@@ -0,0 +1,9 @@
+import 'package:ac_inet_connectivity_checker/ac_inet_connectivity_checker.dart';
+
+Future<void> main() async {
+  final checker = InetConnectivityChecker(
+    endpoint: const InetEndpoint(host: 'google.com', port: 80),
+  );
+
+  print(await checker.cancelableOperation.value);
+}
